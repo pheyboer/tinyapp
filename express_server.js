68453d972d1e02117507af089c85c354be3e8a2c
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-28 20:31:54 -0400

    added express_server.js with detailed notes

--- express_server.js
+++ express_server.js
@@ -1,16 +1,24 @@
-const express = require("express");
-const app = express();
+const express = require("express"); //Import Express module
+const app = express(); //Create an Express Application
 const PORT = 8080; // default port 8080
 
+// Sample URL
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
+// Define route for root URL
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send("Hello!"); // Send greeting as response
 });
 
+// Define route to get the URL database in JSON 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+// Start server and listen on specified port
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Example app listening on port ${PORT}!`); // Log message when server starts
 });
